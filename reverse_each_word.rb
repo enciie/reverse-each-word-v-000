60bdbb53ffa81ffb923bdd2748b6c9b17aa62b1d
Nancy Jo <enciie@gmail.com>
2018-04-30 20:55:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
-  sentence_array.each_with_index do |word, index| word[i].reverse
+  sentence_array.each_with_index do |word, index| word[index].reverse
   end
 end
